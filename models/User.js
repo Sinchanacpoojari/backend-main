d594a9449fe7adcd33aaac6cc374e6a697e5060a
Sinchanacpoojari <sinchanapoojari230@gmail.com>
2025-03-17 17:50:48 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 const UserSchema=mongoose.Schema({
     username:{type:String,required:true},
-    gmail:{type:String,required:true},
+    gmail:{type:String,required:true,unique:true},
     password:{type:String,required:true}
 })
 
