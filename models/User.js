07f10a1a8d7deab1b37b5795f65fadc689728c28
Sinchanacpoojari <sinchanapoojari230@gmail.com>
2025-03-17 17:50:25 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose')
 
 const UserSchema=mongoose.Schema({
-    username:{type:String,require:true},
-    email:{type:String,require:true},
-    password:{type:String,require:true}
+    username:{type:String,required:true},
+    gmail:{type:String,required:true},
+    password:{type:String,required:true}
 })
 
 module.exports=mongoose.model('User',UserSchema)
